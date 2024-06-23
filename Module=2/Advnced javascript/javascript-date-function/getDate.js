2d2c6ca8493d488294ae836291fa1175b2882e0e
Disha_11july_fd <139227572+Dishabhimani@users.noreply.github.com>
2024-06-23 14:01:17 +0530

    Add files via upload

---
+++ Module=2/Advnced javascript/javascript-date-function/getDate.js
@@ -0,0 +1,38 @@
+
+var d=new Date();
+//console.log(d);
+// var res=d.getDate();
+// console.log(res);
+
+// var res=d.getFullYear();
+// console.log(res);
+
+
+// var res=d.getMonth();
+// console.log(res);
+
+// var res=d.getTime();
+// console.log(res);
+
+
+// var res=d.getTimezoneOffset();
+// console.log(res);
+
+
+// var res=new Date();
+// console.log(res);
+
+
+// const str = new Date().toLocaleString('en-US', { timeZone: 'Asia/Calcutta' });
+
+// const str = new Date().toLocaleString('en-US', { timeZone: 'Asia/Dhaka' });
+// console.log(str);
+
+// const str = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kabul' });
+// console.log(str);
+
+// const str = new Date().toLocaleString('en-US', { timeZone: 'Asia/Calcutta' });
+// console.log(str);
+
+const str = new Date().toLocaleString('en-US', { timeZone: 'Asia/Calcutta' });
+console.log(str);
