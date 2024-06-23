711a90f3115b19ea061e8da8e6374c15526bcac0
Disha_11july_fd <139227572+Dishabhimani@users.noreply.github.com>
2024-06-23 14:28:52 +0530

    23jume

---
+++ Module=2/Advnced javascript/to-do-list/js/tailwinds_config.js
@@ -0,0 +1,40 @@
+
+tailwind.config = {
+    theme: {
+    fontSize: {
+    sm: '0.8rem',
+    base: '1rem',
+    xl: '1.25rem',
+    '2xl': '1.563rem',
+    '3xl': '1.953rem',
+    '4xl': '2.441rem',
+    '5xl': '3.052rem',
+    },
+    extend: {
+    animation: {
+    'spin-slow': 'spin 3s linear infinite',
+    },    
+    colors: {
+    clifford: '#da373d',
+    },
+    
+    flexBasis: {
+    '1/7': '14.2857143%',
+    '2/7': '28.5714286%',
+    '3/7': '42.8571429%',
+    '4/7': '57.1428571%',
+    '5/7': '71.4285714%',
+    '6/7': '85.7142857%',
+    },
+    
+    colors: {
+      'regal-blue': '#243c5a',
+    },
+    
+    width: {
+    '128': '32rem',
+    }
+      
+    }
+    }
+    }
